41e74abcdd54b56299a377f46e75a7a5e6ba8948
FarhadF <farhad.farahi@gmail.com>
2017-05-17 20:50:14 +0430

    home page background

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: 'movies',
     component: MoviesComponent
   },
+  { path: '',
+    component: HomeComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
