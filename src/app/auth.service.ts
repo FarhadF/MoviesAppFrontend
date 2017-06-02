30edb34f0cc1dcefe11d735322b8870940098e52
Farhad <farhad.farahi@gmail.com>
2017-06-02 13:09:01 +0430

    fixed login state in navbar and login error alert

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,12 +3,16 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { Headers, RequestOptions } from '@angular/http';
+import { Router } from '@angular/router';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
   baseUrl: string = 'http://127.0.0.1:8080/';
+  
+ 
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, private router: Router) { }
 
   login(body: string): Observable<any> {
     let headers = new Headers({ 'Content-Type': 'application/json' }); 
@@ -17,6 +21,16 @@ export class AuthService {
     return this.http.post(this.baseUrl + 'login', body, options)
 	  .map((res: Response) => res.json());
     
+  }
+  logout() {
+    localStorage.removeItem('token');
+	//this.setLoggedin(false);
+	this.router.navigate(['/']);
+  }
+  
+  get authenticated() {
+   
+    return tokenNotExpired('token');
   }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../movies.service';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +11,18 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent implements OnInit {
   
-  constructor(private authService: AuthService) { }
-
+  constructor(private authService: AuthService, private router: Router) { }
+  errorHandler: boolean
   onSubmit(f) {
     console.log(JSON.stringify(f.value));
 	this.authService.login(JSON.stringify(f.value))
 	  .subscribe(
 	    data => { if (data.token) {
 		    localStorage.setItem('token', data.token);
+			this.router.navigate(['/']);
 		  } else if (data.error) {
 		      console.log(data);
+			  this.errorHandler=true
 		  }
 		  },
 
